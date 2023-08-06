d892e09f93c17866506b4fb90caf64c64be6022f
overflowsatellite <flatside17@gmail.com>
2023-08-07 00:21:14 +0700

    Remove home component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
 import MNavbar from "./components/MNavbar.vue";
+import MJumbotron from "../components/MJumbotron.vue";
+import MIntro from "../components/MIntro.vue";
+import MServices from "../components/MServices.vue";
+import MFlow from "../components/MFlow.vue";
+import MClients from "../components/MClients.vue";
+import MContacts from "../components/MContacts.vue";
 import MFooter from "./components/MFooter.vue";
 </script>
 
 <template>
   <MNavbar />
   <main>
-    <router-view> </router-view>
+    <div class="relative flex flex-col">
+      <MJumbotron />
+      <MIntro />
+      <MServices />
+      <MFlow />
+      <MClients />
+      <MContacts />
+    </div>
   </main>
   <MFooter />
 </template>
