cd8f66bdf260e52f9f2a156cbe2f5127e792e0c1
overflowsatellite <flatside17@gmail.com>
2023-08-07 00:23:15 +0700

    Fix path error

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import MNavbar from "./components/MNavbar.vue";
-import MJumbotron from "../components/MJumbotron.vue";
-import MIntro from "../components/MIntro.vue";
-import MServices from "../components/MServices.vue";
-import MFlow from "../components/MFlow.vue";
-import MClients from "../components/MClients.vue";
-import MContacts from "../components/MContacts.vue";
+import MJumbotron from "./components/MJumbotron.vue";
+import MIntro from "./components/MIntro.vue";
+import MServices from "./components/MServices.vue";
+import MFlow from "./components/MFlow.vue";
+import MClients from "./components/MClients.vue";
+import MContacts from "./components/MContacts.vue";
 import MFooter from "./components/MFooter.vue";
 </script>
 
